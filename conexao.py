0947621e2964c617709fd6555b74b413a712075b
Matheus Eduardo Tavares <matheustavares1165@gmail.com>
2019-12-22 03:44:16 -0200

    Término do desenvolvimento da função que permite cadastrar mais administradores no sistema, com realização do tratamento de dados de forma correta e bem definida

--- conexao.py
+++ conexao.py
@@ -1,9 +1,8 @@
 from tkinter import messagebox
+import pymysql as database
 
 
 def registros_usuarios():
-    import pymysql as database
-
     conexao = database.connect(
         host='localhost',
         user='root',
@@ -23,6 +22,27 @@ def registros_usuarios():
     else:
         return resultado
 
+
+def cadastro_adm(nome, senha, sexo, endereco):
+    conexao = database.connect(
+        host='localhost',
+        user='root',
+        password='',
+        db='projeto_db',
+        charset='utf8mb4',
+        cursorclass=database.cursors.DictCursor
+    )
+
+    try:
+        with conexao.cursor() as sql:
+            sql.execute(f'insert into usuario values (default, "{nome}", "{senha}", "{sexo}", "2", "{endereco}");')
+            conexao.commit()
+    except Exception as erro:
+        messagebox.showinfo('Erro', 'Não foi possível se conectar ao banco de dados')
+        print(erro)
+        exit()
+    else:
+        messagebox.showinfo('Notificação', f'Usuário {nome} cadastrado com sucesso !')
 
 
 
--- window_adm.py
+++ window_adm.py
@@ -27,7 +27,7 @@ class Window():
             botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Pedidos em Andamento', padx=5, pady=5).grid(row=1, column=0, padx=20, pady=20)
             botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Todos os Pedidos', padx=5, pady=5).grid(row=1, column=1, padx=20, pady=20)
             botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Cadastros', padx=5, pady=5, command=self.cadastros).grid(row=2, column=0, padx=20, pady=20)
-            botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Cadastrar Novo Administrador', padx=5, pady=5).grid(row=2, column=1, padx=20, pady=20)
+            botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Cadastrar Novo Administrador', padx=5, pady=5, command=self.front_end_adm).grid(row=2, column=1, padx=20, pady=20)
             botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Gerar Estatísticas', padx=5, pady=5).grid(row=3, column=0, padx=20, pady=20)
             botao.HoverButton(self.layout, width=25, bg='#1960a6', fg='white', font=('Century Gothic bold', 14), text='Voltar', padx=5, pady=5, command=self.sair).grid(row=3, column=1, padx=20, pady=20)
             self.layout.grid(row=1, column=0)
@@ -74,6 +74,54 @@ class Window():
             lista_auxiliar.clear()
 
         self.smallwindow.mainloop()
+
+    def back_end_adm(self):
+        if len(self.n.get()) == 0 or len(self.s.get()) == 0 or len(self.sex.get()) == 0 or len(self.e.get()) == 0:
+            messagebox.showinfo('Erro', 'Algum(ns) do(s) campo(s) está(ão) vazio(s)')
+        elif len(self.s.get()) <= 3:
+            messagebox.showinfo('Erro', 'A senha deve conter ao menos quatro caracteres')
+        else:
+            try:
+                self.genero = self.sex.get()[0].upper()
+                print(self.genero)
+            except:
+                messagebox.showinfo('Erro', 'O campo sexo deve ser preenchido com [M/F]')
+            else:
+                if self.genero != 'M' and self.genero != 'F':
+                    messagebox.showinfo('Erro', 'O campo sexo deve ser preenchido com [M/F]')
+                else:
+                    projeto.conexao.cadastro_adm(self.n.get(), self.s.get(), self.genero, self.e.get())
+
+    def front_end_adm(self):
+        self.tela = Toplevel()
+        self.tela.configure(background='#fbb339')
+        self.tela.title('Cadastro de um Novo Administrador')
+        self.tela.geometry('340x300')
+        self.tela.resizable(False, False)
+
+        self.encapsular = Frame(self.tela, bg='#fbb339')
+        self.encapsular.grid(row=0, column=0)
+
+        Label(self.encapsular, font=('Century Gothic bold', 16), text='Nome', bg='#fbb339', fg='white', padx=30, pady=10).grid(row=0, column=0)
+        self.n = Entry(self.encapsular)
+        self.n.grid(row=0, column=1)
+
+        Label(self.encapsular, font=('Century Gothic bold', 16), text='Senha', bg='#fbb339', fg='white', padx=30, pady=10).grid(row=1, column=0)
+        self.s = Entry(self.encapsular, show='*')
+        self.s.grid(row=1, column=1)
+
+        Label(self.encapsular, font=('Century Gothic bold', 16), text='Sexo', bg='#fbb339', fg='white', padx=30, pady=10).grid(row=2, column=0)
+        self.sex = Entry(self.encapsular)
+        self.sex.grid(row=2, column=1)
+
+        Label(self.encapsular, font=('Century Gothic bold', 16), text='Endereço', bg='#fbb339', fg='white', padx=30, pady=10).grid(row=3, column=0)
+        self.e = Entry(self.encapsular)
+        self.e.grid(row=3, column=1)
+
+        Label(self.encapsular, bg='#fbb339').grid(row=4, column=0)
+        botao.HoverButton(self.encapsular, font=('Century Gothic bold', 16), text='Cadastrar', pady=5, padx=5, fg='white', bg='#d32016', command=self.back_end_adm, activebackground='#1960a6', activeforeground='white').grid(row=4, column=1, pady=10, padx=30)
+
+        self.tela.mainloop()
 
 
 def verifica_login(usuario, senha):
